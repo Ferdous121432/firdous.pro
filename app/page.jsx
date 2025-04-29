a46484810ad397438178c77c71f34170c3d84318
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2025-04-29 17:46:11 +0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
-        <Approach />
+        {/* <Approach /> */}
         <Contact />
         <Footer />
       </div>
